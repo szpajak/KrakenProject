22ecb89aa9b6e1394614bf61bb9924f2c6ae81c5
szpajak <szymon.pajak@gmail.com>
2025-06-11 18:45:07 +0200

    chore: updated structure

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def main():
         st.session_state.chat_history = None
     if "vector_store" not in st.session_state:
         try:
-            df = pd.read_csv('chunks.csv')
+            df = pd.read_csv('data/chunks.csv')
             text_chunks = df['chunk'].tolist()
             st.session_state.vector_store = get_vector_store(text_chunks)
             st.success("Your PDFs have been processed successfully. You can ask questions now.")
