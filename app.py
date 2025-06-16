179118f70d9c46b8d186c8c43f221dfb69956893
szpajak <szymon.pajak@gmail.com>
2025-06-16 22:39:13 +0200

    feat: prompt update and chunking

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL_NAME = "gemini-1.5-flash-8b"
-EMBEDDING_MODEL = "models/embedding-001"
-MIN_KEYWORDS_SCORE = 0.15
-MIN_UNCERTAINTY_SCORE = 0.8
-MIN_CONTEXT_OVERLAP = 0.5
-
 import streamlit as st
 import pandas as pd
 from langchain.text_splitter import CharacterTextSplitter
@@ -21,12 +12,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.callbacks.base import BaseCallbackHandler
 from vertexai.preview import tokenization
 
-from evaluation import ResponseEvaluator
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL_NAME = "gemini-1.5-flash-8b"
+EMBEDDING_MODEL = "models/embedding-001"
 
 # uses https://github.com/google/sentencepiece under the hood, works offline
-tokenizer = tokenization.get_tokenizer_for_model("gemini-1.5-flash-002")
-
-
+tokenizer = tokenization.get_tokenizer_for_model("gemini-1.5-flash-8b")
 
 
 class CustomHandler(BaseCallbackHandler):
@@ -43,10 +35,6 @@ class CustomHandler(BaseCallbackHandler):
 
 
 handler = CustomHandler()
-evaluator = ResponseEvaluator(
-    min_length=20,
-    max_length=50,
-)
 
 
 def get_vector_store(text_chunks):
@@ -63,7 +51,7 @@ def get_conversation_chain(vector_store):
     llm = ChatGoogleGenerativeAI(
         google_api_key=GEMINI_API_KEY,
         model=GEMINI_MODEL_NAME,
-        temperature=0,
+        temperature=0.2,
         callbacks=[handler],
     )
 
@@ -75,10 +63,7 @@ def get_conversation_chain(vector_store):
     system_template = """
         Use the following pieces of context and chat history to answer in English the question at the end.
         In the answer tell exact paragraph number from the context where the answer is found e.g. "According to §3(2) of the AGH Study Regulations, students must submit their course selection within two weeks of the semester start."
-        If the answer have paragraph number (e.g. "§3(2)"), you should include in anserw this document name "AGH Study Regulations".
-        If the answer starts with FACT AND FIGURES or ACADEMIC YEAR SCHEDULE then add those name instead of paragraph number.
         If you don't know the answer, just say that you don't know, don't try to make up an answer.
-        Answer in full sentences.
 
         Context: {context}
 
@@ -110,9 +95,6 @@ def get_conversation_chain(vector_store):
 def handle_user_input(question):
     response = st.session_state.conversation({"question": question})
     st.session_state.chat_history = response["chat_history"]
-    ai_msgs = [msg for msg in st.session_state.chat_history if msg.type == "ai"]
-    last_ai_response = ai_msgs[-1].content if ai_msgs else ""
-    return last_ai_response
 
 
 def display_chat_history():
@@ -143,7 +125,9 @@ def main():
     if "vector_store" not in st.session_state:
         try:
             df = pd.read_csv("data/chunks.csv")
+            # print(df)
             text_chunks = df["chunk"].tolist()
+            # print(text_chunks)
             st.session_state.vector_store = get_vector_store(text_chunks)
             st.success(
                 "Your PDFs have been processed successfully. You can ask questions now."
@@ -162,58 +146,23 @@ def main():
     if prompt := st.chat_input("Ask anything to your PDF:"):
         with st.chat_message("user"):
             st.markdown(prompt)
-        last_ai_response = handle_user_input(prompt)
-        if last_ai_response:
+        handle_user_input(prompt)
+        if st.session_state.chat_history:
+            last_ai_response = [
+                msg for msg in st.session_state.chat_history if msg.type == "ai"
+            ][-1].content
             with st.chat_message("assistant"):
                 st.markdown(last_ai_response)
 
-        if isinstance(st.session_state.retrieved, list):
-            context_text = " ".join(st.session_state.retrieved)
-        else:
-            context_text = str(st.session_state.retrieved)
-
-        eval_res = evaluator.evaluate(prompt, last_ai_response, context_text)
-
-        warning_msgs = []
-        if not eval_res['basic_criteria']:
-            warning_msgs.append(eval_res['message'])
-        else:
-            keyword_score = eval_res['keyword_score']
-            length_score = eval_res['length_score']
-            uncertainty_score = eval_res['uncertainty_score']
-
-            if keyword_score < MIN_KEYWORDS_SCORE:
-                warning_msgs.append(
-                    f"Low keyword match score: {keyword_score}. "
-                )
-            if length_score == -1:
-                warning_msgs.append(
-                    f"Answer is too short."
-                )
-            if length_score == 1:
-                warning_msgs.append(
-                    f"Answer is too long."
-                )
-            if uncertainty_score < MIN_UNCERTAINTY_SCORE:
-                warning_msgs.append(
-                    f"High uncertainty in the answer: {uncertainty_score}. "
-                )
-            if eval_res['context_overlap_score'] < MIN_CONTEXT_OVERLAP:
-                warning_msgs.append(
-                    f"Low context overlap score: {eval_res['context_overlap_score']}. "
-                )
-        for wm in warning_msgs:
-            st.warning(wm)
-
     with st.sidebar:
         st.header("Additional Information")
-
+        
         with st.expander("💸 Token Count"):
             st.code(st.session_state.tokens_used, language="markdown")
 
         with st.expander("💬 Prompt Sent to LLM"):
             st.code(st.session_state.add_info_prompt, language="markdown")
-
+        
         with st.expander("📄 Retrieved Documents"):
             st.markdown("Chunks retrieved by the vector search:")
             st.code(st.session_state.retrieved, language="markdown")
